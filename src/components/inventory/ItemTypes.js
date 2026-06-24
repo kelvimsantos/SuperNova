ee619f9ea94a21324e9632f7743745da74c814a8
kelvim santos <contatoshake@hotmail.com>
2026-06-24 15:23:48 -0300

    melhoria - melhoria , adicionando armas 7

--- src/components/inventory/ItemTypes.js
+++ src/components/inventory/ItemTypes.js
@@ -146,7 +146,7 @@ export const ItemDatabase = {
     modelPath: '/models/weapons/fantasy_sword.glb',
   customPosition: [0.35, -0.15, 0.1],   // [x, y, z]
   customRotation: [90.6, 90, 90.6],         // [x, y, z] em radianos
-  customScale: [0.2, 0.2, 0.2],          // [x, y, z]
+  customScale: [1.2, 1.2, 1.2],          // [x, y, z]
   },
 
   
